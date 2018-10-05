1a408cd7f3b97f096fe11f83f72d9de61e72fb0d
Wojtek <wojciech.olasiewicz@mobica.com>
2018-10-05 12:44:43 +0200

    Initial Commit

---
+++ ListToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListToDo
+//
+//  Created by Olasiewicz,  Wojciech on 10/5/18.
+//  Copyright © 2018 Olasiewicz,  Wojciech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
